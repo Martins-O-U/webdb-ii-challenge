67b8621818568c192fe81735dbec77eb3e5a9977
Martins-O-U <martinsonyedikachi@gmail.com>
2019-11-05 19:18:45 +0100

    Created a post and deleted function

--- router/car-router.js
+++ router/car-router.js
@@ -4,6 +4,14 @@ const router = express.Router();
 
 const db = require("../data/db-config");
 
+function FieldValidator(req, res, next) {
+    if (req.body.VIN && req.body.Make && req.body.Model && req.body.Mileage) {
+        next();
+    } else {
+        res.status(400).json({ error: "please provide all the needed values (VIN, Make,Model,Milage)" })
+    }
+}
+
 
 router.get("/", (req, res) => {
     db("cars")
@@ -11,7 +19,30 @@ router.get("/", (req, res) => {
             res.status(200).json(cars)
         })
         .catch(error => {
-            res.status(500).json({ error: "Something went wrong " + error.message })
+            res.status(500).json({ error: "Something went wrong with:- " + error.message })
+        })
+})
+
+router.post("/", FieldValidator, (req, res) => {
+    db("cars").insert(req.body)
+        .then(car => {
+            res.json('New Car profile got created with an id of ' + car[0]);        })
+        .catch(error =>{
+            res.status(500).json({ error: "Something went wrong with:- " + error.message })
+        })
+})
+
+router.delete('/:id', (req, res)=>{
+    db('cars').where({id : req.params.id}).del()
+        .then(deleted =>{
+            if(deleted === 1){
+                res.json(deleted + " Car infomation got DELETED!")
+            }else{
+                res.status(400).json({message: "Car profile with that specific ID does not exist in the DataBase"})
+            } 
+        })
+        .catch (error => {
+            res.status(500).json({ message: 'Something went wrong with:- ' + error.message });
         })
 })
 
